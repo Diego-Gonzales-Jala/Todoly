ee219cd667e367cf459079eb28f8b21c5eb6449c
BQE-DGS <bqe.dgs@gmail.com>
2017-11-23 16:06:30 -0400

    add script in the util folder

--- features/utils/apiRest.py
+++ features/utils/apiRest.py
@@ -1,7 +1,20 @@
 import requests
+from features.utils.file_system import *
+
+data = FileSystem('../data/data_user.json')
+
 header = {'Authorization':'Basic cWEudGVzdGluZy5kZ3NAZ21haWwuY29tOmRpZWdvMTk4Nw=='}
 r = requests.get('https://todo.ly/api/user.json', headers=header)
 
-print("Values \n", r.json())
-print("Context \n", r.content)
-print("Code \n", r.status_code)
+result = r.json()
+#print(result['Id'])
+#print(result['Email'])
+
+print(data._get_DataJson())
+
+
+op1 = data._get_DataJson()
+print(data._compare_to(op1, r.json()))
+data._print_structure_json(op1)
+
+data._cleanup()
