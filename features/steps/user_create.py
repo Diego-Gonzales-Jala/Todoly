6b9003e80192cc25942e577f809f9e6f49f29383
marceines <chelita141@gmail.com>
2017-11-23 19:19:56 -0400

    user changes

--- features/steps/user_create.py
+++ features/steps/user_create.py
@@ -1,20 +1,23 @@
 from compare import *
 from utils.module_rest import *
 from behave import given, when, then
-import requests
 import json
 
-@given(u'I have create request {payload}')
-def step_impl(context, payload):
-    context.payload = payload
+@given(u'I have a service for "{servicemethodpost}" to create user')
+def step_impl(context,servicemethodpost ):
+    context.servicemethodpost = servicemethodpost
+
+@given(u'I have create request payload')
+def step_impl(context):
+    context.payloaduser = json.loads(context.text)
 
 @when(u'I send a {methodpost} request to create a new user')
 def step_impl(context, methodpost):
-    context.methodpost = methodpost
-    context.url      = context.host + context.rootPath + context.servicemethod
-    context.createParams2 = {"Email":"test.testi@jalasoft.com", "FullName": "Irina Torrico","Password": "123456"}
-    context.response = requests.post(context.url, json=context.createParams2)
-    generateFileJson("data/", "data_create_user", context.response.json())
+    context.method   = methodpost
+    context.url      = context.host + context.rootPath + context.servicemethodpost
+    context.pyloadrequest = context.payloaduser
+    context.response = requests.post(context.url, json=context.pyloadrequest)
+    generateFileJson("data/", "User_POST", context.response.json())
 
 
 @then(u'I receive status code {status_code} for the response create user')
