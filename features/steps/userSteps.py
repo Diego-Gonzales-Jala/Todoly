24ba48414e99d46edddc12c192dfbb33b08f698e
marceines <chelita141@gmail.com>
2017-11-23 15:06:19 -0400

    test
    test

--- features/steps/userSteps.py
+++ features/steps/userSteps.py
@@ -1,22 +1,24 @@
 from compare import *
 from utils.module_rest import *
-from behave import given, when, then
 
-@given(u'I have a service for "{servicemethod}" for user')
-def step_impl(context,servicemethod ):
-    context.servicemethod = servicemethod
+@given(u'I have a user authenticated')
+def step_impl(context):
+    pass
 
-@when(u'I send {method} user request to get user information')
+@given(u'I have service for "{serviceMethod}"')
+def step_impl(context, serviceMethod ):
+    context.serviceMethod = serviceMethod
+
+@when(u'I send {method} user request to have user information')
 def step_impl(context, method):
-    context.method   = method
-    context.url      = context.host + context.rootPath + context.servicemethod
-    context.headers  = context.token
-    context.response = perform_request(context.method, context.url, context.headers)
-    generateFileJson("data/", "data_get_user", context.response.json())
+    context.method = method
+    url = context.host + context.rootPath + context.serviceMethod
+    headers = context.token
+    context.response = perform_request(context.method, url, headers)
+
+    print(context.response.json())
+    generateFileJson("data/", "data_user", context.response.json())
 
 @then(u'I receive status code {status_code} for the response')
 def step_impl(context, status_code):
-    expect(context.response.status_code).to_equal(int(status_code))
-
-
-
+    expect(int(status_code)).to_equal(context.response.status_code)
